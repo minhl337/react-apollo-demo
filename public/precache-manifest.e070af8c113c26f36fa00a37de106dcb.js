374eb342313860375d1f39b2acea5a3ee62bb967
Minh Chau Ha <imnotminh@gmail.com>
2020-06-14 15:33:42 -0500

    heroku deployment

---
+++ public/precache-manifest.e070af8c113c26f36fa00a37de106dcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8aaed7be7ff89268a90ef3d63fcff5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "562c1807a991b8a7a8ec",
+    "url": "/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "d05b666b33ba26f2aa81",
+    "url": "/static/js/2.c68a8871.chunk.js"
+  },
+  {
+    "revision": "f0d43bb23094b147d2c500466b241ef2",
+    "url": "/static/js/2.c68a8871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "562c1807a991b8a7a8ec",
+    "url": "/static/js/main.58be999b.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "f2b1b847235119d0ae094a606c74fca4",
+    "url": "/static/media/share.f2b1b847.jpg"
+  }
+]);
